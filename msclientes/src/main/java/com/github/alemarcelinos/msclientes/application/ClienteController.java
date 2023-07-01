7c1b31f32c16b58c3b75752ca82d2010375fbfd9
Alexandre Marcelino <amsantos9@gmail.com>
2023-07-01 15:05:34 -0300

    implementing create and getbycpf endpoints

--- msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/ClienteController.java
+++ msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/ClienteController.java
@@ -1,19 +1,45 @@
 package com.github.alemarcelinos.msclientes.application;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.alemarcelinos.msclientes.application.dto.ClienteSaveRequest;
+import com.github.alemarcelinos.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "clientes")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ClienteController {
 
     private final ClienteService clienteService;
 
     @GetMapping
     public String status(){
-        return "Ok";
+        return "OK";
+    }
+
+    @PostMapping
+    public ResponseEntity saveClient(@RequestBody ClienteSaveRequest request){
+       Cliente clienteDto = request.toModel();
+       clienteService.save(clienteDto);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(clienteDto.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity findByCpf(@RequestParam("cpf") String cpf){
+        Optional<Cliente> cliente = clienteService.getByCpf(cpf);
+        if(cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 }
---
+++ msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/dto/ClienteSaveRequest.java
@@ -0,0 +1,16 @@
+package com.github.alemarcelinos.msclientes.application.dto;
+
+import com.github.alemarcelinos.msclientes.domain.Cliente;
+import lombok.Data;
+
+@Data
+public class ClienteSaveRequest {
+
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel(){
+        return new Cliente(cpf, nome, idade);
+    }
+}
