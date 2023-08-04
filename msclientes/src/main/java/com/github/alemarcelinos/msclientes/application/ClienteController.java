f5f22134d709861d3be557a754d23cf13eaeffc5
Alexandre Marcelino <amsantos9@gmail.com>
2023-08-04 13:56:38 -0300

    adding changes in the structure the code

--- msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/ClienteController.java
+++ msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/ClienteController.java
@@ -2,6 +2,7 @@ package com.github.alemarcelinos.msclientes.application;
 
 import com.github.alemarcelinos.msclientes.application.dto.ClienteDTO;
 import com.github.alemarcelinos.msclientes.application.impl.ClienteServiceImpl;
+import com.github.alemarcelinos.msclientes.infra.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +24,17 @@ public class ClienteController {
         return "OK";
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ClienteDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), ClienteDTO.class));
+    }
+
     @PostMapping
     public ResponseEntity<ClienteDTO> saveClient(@RequestBody ClienteDTO obj){
         URI headerLocation = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .query("cpf={cpf}")
-                .buildAndExpand(service.save(obj))
+                .buildAndExpand(mapper.map(service.save(obj), ClienteDTO.class))
                 .toUri();
         return ResponseEntity.created(headerLocation).build();
     }
@@ -36,10 +42,5 @@ public class ClienteController {
     @GetMapping(params = "cpf")
     public ResponseEntity<ClienteDTO> findByCpf(@RequestParam("cpf") String cpf){
         return ResponseEntity.ok().body(mapper.map(service.findByCpf(cpf), ClienteDTO.class));
-    }
-
-    @PutMapping(value = "/{cpf}")
-    public ResponseEntity<ClienteDTO> updateCliente(@PathVariable String cpf, @RequestBody ClienteDTO obj){
-        return ResponseEntity.ok().body(mapper.map(service.update(obj), ClienteDTO.class));
     }
 }
--- msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/utils/config.java
+++ msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/utils/config.java
@@ -1,14 +1,30 @@
 package com.github.alemarcelinos.msclientes.application.utils;
 
+import com.github.alemarcelinos.msclientes.domain.Cliente;
+import com.github.alemarcelinos.msclientes.infra.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@AllArgsConstructor
 public class config {
+
+    private ClienteRepository repository;
 
     @Bean
     public ModelMapper mapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public void startDB() {
+        Cliente cli1 = new Cliente(null,"001.123.321-00", "Alexandre", 28);
+        Cliente cli2 = new Cliente(null,"001.123.321-02", "Marília", 30);
+
+        repository.saveAll(List.of(cli1, cli2));
     }
 }
