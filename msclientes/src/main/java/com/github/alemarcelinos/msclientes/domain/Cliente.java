b450b466c32fa903f38535bd51d371d731269e41
Alexandre Marcelino <amsantos9@gmail.com>
2023-08-04 13:55:25 -0300

    adding bean validation

--- msclientes/src/main/java/com/github/alemarcelinos/msclientes/domain/Cliente.java
+++ msclientes/src/main/java/com/github/alemarcelinos/msclientes/domain/Cliente.java
@@ -3,10 +3,7 @@ package com.github.alemarcelinos.msclientes.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -16,11 +13,14 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String cpf;
     private String nome;
     private Integer idade;
 
-    public Cliente(String cpf, String nome, Integer idade) {
+    public Cliente(Long id, String cpf, String nome, Integer idade) {
+        this.id = id;
         this.cpf = cpf;
         this.nome = nome;
         this.idade = idade;
