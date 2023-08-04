35daa9e53c7478c3c325dc4afd3fbd3a0f466f69
Alexandre Marcelino <amsantos9@gmail.com>
2023-08-04 16:52:06 -0300

    removing unused imports

--- msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/dto/ClienteDTO.java
+++ msclientes/src/main/java/com/github/alemarcelinos/msclientes/application/dto/ClienteDTO.java
@@ -3,9 +3,6 @@ package com.github.alemarcelinos.msclientes.application.dto;
 import com.github.alemarcelinos.msclientes.domain.Cliente;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.UniqueElements;
-
-import javax.persistence.Column;
 
 @Data
 @RequiredArgsConstructor
