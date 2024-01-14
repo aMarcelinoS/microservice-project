39846a0d0d37f169b51623a2ea29f5fb94722b08
Alexandre Marcelino <amsantos9@gmail.com>
2024-01-14 12:47:02 -0300

    card domain implemented

--- mscartoes/src/main/java/com/github/alexandre/mscartoes/domain/Card.java
+++ mscartoes/src/main/java/com/github/alexandre/mscartoes/domain/Card.java
@@ -1,21 +1,27 @@
 package com.github.alexandre.mscartoes.domain;
 
+import com.github.alexandre.mscartoes.domain.enums.FlagCard;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class Card {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String flag;
-    private BigDecimal limit;
-    private BigDecimal income;
 
+    @Enumerated(EnumType.STRING)
+    private FlagCard flagCard;
+    private BigDecimal cardLimit;
+    private BigDecimal income;
 }
